ecfd1c797816a7d9a71f94c98f3317fc03c45c51
Yoga Meleniawan Pamungkas <64576201+yogameleniawan@users.noreply.github.com>
2024-09-08 14:22:55 +0700

    style: page center component py

--- components/atoms/PageCenter.tsx
+++ components/atoms/PageCenter.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const PageCenter = ({ children }: { children: React.ReactNode}) => {
   return (
-    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+    <section className="flex flex-col items-center justify-center gap-4">
       {children}
     </section>
   );
